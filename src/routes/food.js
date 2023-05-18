6655faeee089d50efbf8591ceb87f310d8acdc28
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-05-18 11:59:28 -0400

    CRUD and tests are up and running

--- src/routes/food.js
+++ src/routes/food.js
@@ -5,26 +5,39 @@ const router = express.Router();
 
 const { foodModel } = require('../models');
 
+router.post('/food', async (req, res, next) => {
+
+  let newFood = await foodModel.create(req.body);
+  res.status(200).send(newFood);
+});
+
 router.get('/food', async (req, res, next) => {
   
   let foods = await foodModel.findAll();
   res.status(200).send(foods);
-
 });
 
 router.get('/food/:id', async (req, res, next) => {
 
   let singleFood = await foodModel.findAll({where: {id: req.params.id}});
   res.status(200).send(singleFood);
-
 });
 
+router.put('/food/:id', async (req, res, next) => {
 
-router.post('/food', async (req, res, next) => {
+  let updateResponse = await foodModel.update(req.body, {where: {id: req.params.id}});
+  console.log(updateResponse);
+  updateResponse[0] ?
+    res.status(200).send('Updated')
+    :res.status(404).send('Not found');
+});
 
-  let newFood = await foodModel.create(req.body);
-  res.status(200).send(newFood);
+router.delete('/food/:id', async (req, res, next) => {
 
+  let deleteResponse = await foodModel.destroy({where: {id: req.params.id}});
+  deleteResponse ?
+    res.status(200).send('Deleted')
+    :res.status(404).send('Not found');
 });
 
 module.exports = router;
